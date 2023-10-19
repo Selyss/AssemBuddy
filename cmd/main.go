0e565d857fe35c34781551567be1b965e8cc58f0
Selyss <99344963+Selyss@users.noreply.github.com>
2023-10-19 17:48:41 -0400

    feat: update

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"log"
+
 	"github.com/Selyss/chtsht/pkg/chtsht"
 )
 
 func main() {
-	chtsht.QueryASM()
+	// accept lang cli args
+	flag.Usage = func() {
+		fmt.Printf(`
+    Usage:
+
+        cued [OPTIONS|QUERY]
+
+    Options:
+
+        QUERY                   process QUERY and exit
+
+        --help                  show this help page
+        --shell [LANG]          shell mode (open LANG if specified)
+        
+        --config [PATH]         set language option path
+
+        --standalone-install [DIR|help]
+                                install cheat.sh in the standalone mode
+                                (by default, into ~/.cheat.sh/)
+
+    `)
+	}
+
+	config, err := chtsht.NewConfig()
+
 }
--- go.mod
+++ go.mod
@@ -3,10 +3,11 @@ module github.com/Selyss/chtsht
 go 1.18
 
 require (
-	github.com/dustin/go-humanize v1.0.1 // indirect
-	github.com/kataras/tablewriter v0.0.0-20180708051242-e063d29b7c23 // indirect
-	github.com/lensesio/tableprinter v0.0.0-20201125135848-89e81fc956e7 // indirect
+	github.com/hellflame/argparse v1.11.0
+	github.com/olekukonko/tablewriter v0.0.5
+)
+
+require (
 	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
 )
