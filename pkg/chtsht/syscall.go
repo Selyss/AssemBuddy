ab6da76b0a21fb0f549e991cfe2651f7ee0bba7b
Selyss <99344963+Selyss@users.noreply.github.com>
2023-10-18 23:06:49 -0400

    feat: working table generator

--- pkg/chtsht/syscall.go
+++ pkg/chtsht/syscall.go
@@ -3,25 +3,34 @@ package chtsht
 import (
 	"encoding/json"
 	"fmt"
-	"os"
-
 	"github.com/olekukonko/tablewriter"
+	"os"
 )
 
-func DisplaySyscall(syscallJSON []byte) error {
-	jsonData := string(syscallJSON)
-	var syscalls []Syscall
-	fmt.Println(jsonData)
-	err := json.Unmarshal([]byte(jsonData), &syscalls)
-	if err != nil {
-		return err
+func DisplaySyscall(syscallJSON []byte) {
+	var data map[string]interface{}
+	if err := json.Unmarshal(syscallJSON, &data); err != nil {
+		fmt.Println("Error parsing JSON:", err)
+		return
 	}
 
 	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"NR", "SYSCALL", "RAX", "rdi", "rsi", "rdx", "r10", "r8", "r9"})
-	for _, syscall := range syscalls { // HACK: needed?
-		table.Append([]string{syscall.Arch, string(syscall.Nr), syscall.Name, syscall.Refs, syscall.Return, syscall.Arg0, syscall.Arg1, syscall.Arg2, syscall.Arg3, syscall.Arg4, syscall.Arg5})
-	}
+	table.SetHeader([]string{"ARCH", "NR", "SYSCALL", "RAX", "rdi", "rsi", "rdx", "r10", "r8", "r9"})
+
+	// Extract and add data to the table
+	table.Append([]string{
+		fmt.Sprintf("%v", data["arch"]),
+		fmt.Sprintf("%v", data["nr"]),
+		fmt.Sprintf("%v", data["name"]),
+		fmt.Sprintf("%v", data["return"]),
+		fmt.Sprintf("%v", data["arg0"]),
+		fmt.Sprintf("%v", data["arg1"]),
+
+		fmt.Sprintf("%v", data["arg2"]),
+		fmt.Sprintf("%v", data["arg3"]),
+		fmt.Sprintf("%v", data["arg4"]),
+		fmt.Sprintf("%v", data["arg5"]),
+	})
+
 	table.Render()
-	return nil
 }
